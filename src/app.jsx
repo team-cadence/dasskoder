a961ff85c8c3016b073a68897ecc5636892cce1d
Mads Røskar <madshvero@gmail.com>
2018-04-27 19:09:06 +0200

    Fetch codes from firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import * as firebase from 'firebase';
 
 import Navbar from './navbar.jsx';
 import Home from './home.jsx';
 import AddDasskode from './add-dasskode.jsx';
 
 import './app.css';
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+};
+
+const app = firebase.initializeApp(config);
+const database = app.database();
 
 class App extends Component {
   render() {
@@ -15,7 +28,7 @@ class App extends Component {
           <Navbar />
           <div className="container">
             <Switch>
-              <Route exact path="/" component={Home} />
+              <Route exact path="/" render={() => <Home database={database} />}  />
               <Route path="/new" component={AddDasskode} />
             </Switch>
           </div>
--- src/home.jsx
+++ src/home.jsx
@@ -2,32 +2,33 @@ import * as React from "react";
 
 import Dasskode from './dasskode.jsx';
 
-const dasskoder = [
-  {
-    name: "Dasslokket",
-    code: "0604",
-    location: { lat: 59.9102138, lon: 10.7524852 }
-  },
-  {
-    name: "Test",
-    code: "ananas",
-    location: { lat: 59.92, lon: 10.77 }
+export default class Home extends React.PureComponent {
+
+  state = {
+    dasskoder: []
+  };
+
+  componentDidMount() {
+    this.props.database.ref("/locations").on("value", (snap) => {
+      this.setState({dasskoder: snap.val()})
+    });
   }
-];
 
-const Home = () => (
-  <React.Fragment>
-    <h1>Dasskoder</h1>
-    <section id="koder">
-      <ul className="list-group">
-        {dasskoder.map(dasskode => (
-          <li key={dasskode.name} className="list-group-item">
-            <Dasskode {...dasskode} />
-          </li>
-        ))}
-      </ul>
-    </section>
-  </React.Fragment>
-);
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Dasskoder</h1>
+        <section id="koder">
+          <ul className="list-group">
+            {this.state.dasskoder.map(dasskode => (
+              <li key={dasskode.name} className="list-group-item">
+                <Dasskode {...dasskode} />
+              </li>
+            ))}
+          </ul>
+        </section>
+      </React.Fragment>
+    );
+  }
+}
 
-export default Home;
